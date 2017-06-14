b5369cefd66de31f12d927bb36605f8d4b3ffc70
nalhoussari <nalhoussari@hotmail.com>
2017-06-14 16:09:27 -0700

    submitting assignment after having two sections of photos

---
+++ InstaKilo/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  InstaKilo
+//
+//  Created by Noor Alhoussari on 2017-06-14.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Header : UICollectionReusableView
+
+@end
---
+++ InstaKilo/Header.m
@@ -0,0 +1,13 @@
+//
+//  Header.m
+//  InstaKilo
+//
+//  Created by Noor Alhoussari on 2017-06-14.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import "Header.h"
+
+@implementation Header
+
+@end
